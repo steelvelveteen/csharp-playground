1762085efc681f7a6168aa041397264b5f674a30
steelvelveteen <joeyvico@gmail.com>
2020-10-16 08:10:46 +1030

    An example using linq extensions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp_playground
 {
@@ -6,7 +8,21 @@ namespace csharp_playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Usage of linq queries using extensions");
+            QueryOverStringWithExtensionMethods();
+            Console.ReadLine();
+        }
+
+        static void QueryOverStringWithExtensionMethods()
+        {
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+            IEnumerable<string> subset = currentVideoGames.Where(g => g.Contains(" ")).OrderBy(g => g).Select(g => g);
+
+            foreach (string s in subset)
+            {
+                Console.WriteLine("Video game: {0}", s);
+            }
+
         }
     }
 }
