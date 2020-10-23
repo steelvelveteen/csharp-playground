d7359b55167f381057410630dd131d90275e63c6
steelvelveteen <joeyvico@gmail.com>
2020-10-24 08:00:54 +1030

    Simple Dictionary key value pair

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp_playground
 {
@@ -6,7 +7,23 @@ namespace csharp_playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IDictionary<string, string> superheroes = new Dictionary<string, string>();
+
+            superheroes.Add("Clark Kent", "Superman");
+            superheroes.Add("Bruce Wayne", "Batman");
+            superheroes.Add("Tony Stark", "Iron Man");
+
+            Console.WriteLine($"Number of superheroes: {superheroes.Count}");
+
+            // Retrieve one
+            superheroes.TryGetValue("Tony Stark", out string selection);
+            Console.WriteLine($"Selected superheroe: {selection}");
+
+            // Traversing through dictionary list
+            foreach (KeyValuePair<string, string> heroe in superheroes)
+            {
+                Console.WriteLine($"Hereo {heroe.Value} hides under {heroe.Key}");
+            }
         }
     }
 }
