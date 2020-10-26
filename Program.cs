3e2c4ce1ec6563c12575fc87b99fc9e47245699f
steelvelveteen <joeyvico@gmail.com>
2020-10-26 10:44:44 +1030

    Simple example of anonymous and lambda expression on delegates

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@ namespace csharp_playground
 {
     class Program
     {
+        delegate int Operation(int a, int b);
+        delegate void Operation2(int a, int b);
+        delegate int Operation3(int a, int b);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Operation op = Add;
+            int result = op.Invoke(4, 5);
+            Console.WriteLine($"4 + 5 = {result}");
+
+            // Using anonymous method
+            Operation2 op2 = delegate (int a, int b)
+            {
+                Console.WriteLine(a - b);
+            };
+            op2(10, 5);
+
+            // Combining anonymous method with lambda expression
+            Operation3 op3 = (a, b) => a * b;
+            int result3 = op3(5, 4);
+            Console.WriteLine($"5 * 4 = {result3}");
+
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
