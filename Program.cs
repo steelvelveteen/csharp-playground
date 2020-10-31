ae57c82be163c3058ad2b08086be4a8af2035275
steelvelveteen <joeyvico@gmail.com>
2020-10-31 12:06:19 +1030

    Added a simple struct in C#

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace csharp_playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rec = new Rectangle();
+            rec.Length = 10;
+            rec.Height = 20;
+            Console.WriteLine($"Area of rec: {rec.Area()}");
+        }
+    }
+    // Not much to learn except for one key thing:
+    // structs are value types and so they are stored
+    // in the stack as opposed to classes which are ref
+    // values and stored in the heap
+    public struct Rectangle
+    {
+        public double Length { get; set; }
+        public double Height { get; set; }
+
+        public readonly double Area()
+        {
+            return Length * Height;
         }
     }
 }
