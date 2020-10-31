67bdaa80f4f5e16dfa0f5422d4db7a7c9da17a89
steelvelveteen <joeyvico@gmail.com>
2020-10-31 12:42:25 +1030

    Switch expression syntax example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System;
 
 namespace csharp_playground
 {
@@ -6,7 +8,55 @@ namespace csharp_playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var result = DoMath(5,5, MathType.Multiply);
+            var result = DoModernMath(5, 5, MathType.Multiply);
+            Console.WriteLine(result);
+        }
+        enum MathType
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        // Traditional switch
+        static double DoMath(double x, double y, MathType mathType)
+        {
+            double output = 0;
+            switch (mathType)
+            {
+                case MathType.Add:
+                    output = x + y;
+                    break;
+                case MathType.Subtract:
+                    output = x - y;
+                    break;
+                case MathType.Multiply:
+                    output = x * y;
+                    break;
+                case MathType.Divide:
+                    output = x / y;
+                    break;
+            }
+            return output;
+        }
+
+        //  C# 8.0 switch expression
+        // This is very similar to a match or if expression
+        // as right hand value in Rust. Same thing!!
+        // Really cool feature
+        static double DoModernMath(double x, double y, MathType mathType)
+        {
+            var output = mathType switch
+            {
+                MathType.Add => x * y,
+                MathType.Subtract => x - y,
+                MathType.Multiply => x * y,
+                MathType.Divide => x / y,
+                _ => throw new Exception("Bad math type passed in")
+            };
+
+            return output;
         }
     }
 }
