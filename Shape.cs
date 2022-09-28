60d27a536b1302ae9020ba98619382d6344d9b4f
Joey Vico <joeyvico@gmail.com>
2022-09-29 00:34:34 +0930

    Documented difference between virtual and abstract methods

--- Shape.cs
+++ Shape.cs
@@ -5,6 +5,13 @@ namespace csharp_playground
     public abstract class Shape
     {
         public string Name { get; set; } = "Default shape name";
+        /*     
+         Virtual methods have an implementation and provide the derived classes
+         with the option of overriding it. Abstract methods do not provide an 
+         implementation and force the derived classes to override the method. 
+         So, abstract methods have no actual code in them, and (non-abstract) 
+         subclasses HAVE TO override the method.
+         */
 
         public virtual void GetInfo()
         {
