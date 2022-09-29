3d238ec317bd19bb05ef8fa5e2be2a53b2f1539c
steelvelveteen <joeyvico@gmail.com>
2022-09-29 10:45:09 +0200

    reviewing and documenting

--- Program.cs
+++ Program.cs
@@ -3,32 +3,33 @@ using System.Collections.Generic;
 
 namespace csharp_playground
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            IDictionary<string, string> superheroes = GetSuperHeroes();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IDictionary<string, string> superheroes = GetSuperHeroes();
 
-            Console.WriteLine($"Number of superheroes: {superheroes.Count}");
+			Console.WriteLine($"Number of superheroes: {superheroes.Count}");
 
-            // Retrieve one
-            superheroes.TryGetValue("Tony Stark", out string selection);
-            Console.WriteLine($"Selected superheroe: {selection}");
+			// Retrieve one
+			superheroes.TryGetValue("Tony Stark", out string selection);
+			Console.WriteLine($"Selected superheroe: {selection}");
 
-            // Traversing through dictionary list
-            foreach (KeyValuePair<string, string> heroe in superheroes)
-            {
-                Console.WriteLine($"Hereo {heroe.Value} hides under {heroe.Key}");
-            }
-        }
+			// Traversing through dictionary list
+			// This is the standard way
+			foreach (KeyValuePair<string, string> heroe in superheroes)
+			{
+				Console.WriteLine($"Hereo {heroe.Value} hides under {heroe.Key}");
+			}
+		}
 
-        public static IDictionary<string, string> GetSuperHeroes()
-        {
-            return new Dictionary<string, string> {
-                { "Clark Kent", "Superman" },
-                { "Bruce Wayne", "Batman" },
-                { "Tony Stark v", "Iron Man" }
-            };
-        }
-    }
+		public static IDictionary<string, string> GetSuperHeroes()
+		{
+			return new Dictionary<string, string> {
+				{ "Clark Kent", "Superman" },
+				{ "Bruce Wayne", "Batman" },
+				{ "Tony Stark v", "Iron Man" }
+			};
+		}
+	}
 }
